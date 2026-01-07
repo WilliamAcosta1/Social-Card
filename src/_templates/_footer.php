e3de9cd539ba992bdd968481df13211f560540ac
WilliamAcosta1 <williamacosta0014@gmail.com>
2026-01-08 00:44:32 +0100

    Actualiza la URL de GitHub y el nombre en el pie de página a WilliamAcosta.

--- src/_templates/_footer.php
+++ src/_templates/_footer.php
@@ -1,11 +1,11 @@
 <?php
-  $GITHUB_URL = "https://github.com/AlexFlipnote/alexflipnote.github.io";
+  $GITHUB_URL = "https://github.com/WilliamAcosta1/Social-Card";
 ?>
 
 <footer class="text-center box-container primary-bg">
   <p className="no-margin">
     <a href="<?php echo "{$GITHUB_URL}/blob/master/LICENSE"; ?>" target="_blank" rel="noreferrer">LICENSE</a>
-      <span className="footer-name">AlexFlipnote</span>
+      <span className="footer-name">WilliamAcosta</span>
     <a href="<?php echo "{$GITHUB_URL}"; ?>" target="_blank" rel="noreferrer">Source</a>
   </p>
 </footer>
